18c68959362c7f0aa70341ee60956e1f00308d2a
Death1clown <goviner9723@gmail.com>
2025-07-16 17:19:46 +0100

    Add files via upload

---
+++ Version & Installation.bat
@@ -0,0 +1,14 @@
+@echo off
+echo Checking Git version...
+
+git --version >nul 2>&1
+
+IF %ERRORLEVEL% EQU 0 (
+    git --version
+    echo Git is installed and working correctly.
+) ELSE (
+    echo Git is not installed or not added to your PATH.
+    echo Please install Git from https://git-scm.com/downloads
+)
+
+pause
